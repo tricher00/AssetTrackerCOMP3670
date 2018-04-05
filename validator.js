86824376032ee2548bd41cea2ffba85e236c2ea6
tricher00 <tricher00@gmail.com>
2018-04-04 20:47:49 -0400

    Josh's stuff

---
+++ validator.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var formTag = document.getElementByTagName("form");
+    formTag.addEventListener("submit", function(e){
+        var incomplete = false;
+        var req = document.getElementsById("required");
+        for (var i = 0; i < req.length; i){
+            if (req[i].value.length == 0){
+                incomplete = true;
+                req[i].class = 'incomplete'
+            }   
+        }
+        if (incomplete){
+            e.preventDefault();
+        }
+    });
+}, false);
